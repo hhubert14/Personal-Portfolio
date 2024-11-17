9c87247ebc3ceb4f4dea3a141e3e7e59eb7ce937
hhubert14 <h.hubert@wustl.edu>
2024-11-17 02:33:33 -0600

    In the process of implementing dark toggle from https://www.youtube.com/watch?v=7zqI4qMDdg8

--- personal-website/src/app/layout.tsx
+++ personal-website/src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import { Cormorant_Garamond, Poppins } from "next/font/google"
 import "./globals.css";
 import Link from "next/link";
+import DarkModeToggle from "@/components/DarkModeToggle";
+import Providers from "@/app/Providers";
 
 const cormorantGaramond = Cormorant_Garamond({
   subsets: ["latin"],
@@ -39,25 +41,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${cormorantGaramond.variable} ${poppins.variable} ${geistSans.variable} ${geistMono.variable} bg-amber-100 antialiased`}
-      >
-        <nav className="bg-gray-900 p-4">
-          <div className="flex">
-            <ul className="flex justify-between w-2/3">
-              <li><Link href="/" className="text-white">Home</Link></li>
-            </ul>
-            <ul className="flex justify-between w-1/3">
-              <li><Link href="/about" className="text-white">About</Link></li>
-              <li><Link href="/blogs" className="text-white">Blogs</Link></li>
-              <li><Link href="/projects" className="text-white">Projects</Link></li>
-              <li><Link href="/contact" className="text-white">Contact</Link></li>
-            </ul>
-          </div>
-          
-        </nav>
-        {children}
-      </body>
+      <Providers>
+        <body
+          className={`${cormorantGaramond.variable} ${poppins.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <nav className="p-4">
+            <div className="flex">
+              <ul className="flex justify-between w-2/3">
+                <li><Link href="/">Home</Link></li>
+              </ul>
+              <ul className="flex justify-between w-1/3">
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="/blogs">Blogs</Link></li>
+                <li><Link href="/projects">Projects</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
+                <li><DarkModeToggle /></li>
+              </ul>
+            </div>
+          </nav>
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
