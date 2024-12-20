09104e3e98b378d80b084231df34f23576268483
hhubert14 <h.hubert@wustl.edu>
2024-12-19 19:16:46 -0600

    reverted StarBackground.tsx to original version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,12 +75,10 @@
 //   ssr: false,
 // });
 
-import StarsCanvas from "./StarsCanvasClient";
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import StarsCanvas from "@/components/main/StarBackground";
+import StarsCanvas from "@/components/main/StarBackground";
 // import Navbar from "@/components/main/Navbar";
 // import Footer from "@/components/main/Footer";
 
--- src/components/main/StarBackground.tsx
+++ src/components/main/StarBackground.tsx
@@ -1,57 +1,54 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import * as THREE from 'three';
 import { Points, PointMaterial } from "@react-three/drei";
-// import * as random from "maath/random/dist/maath-random.esm";
-// @ts-expect-error: TypeScript cannot find module declaration for 'maath/random/dist/maath-random.esm'
+// @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
-
-
-const StarBackground = (props: JSX.IntrinsicElements['group']) => {
-  const ref = useRef<THREE.Points>(null);
+const StarBackground = (props: any) => {
+  const ref: any = useRef();
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
 
-  useFrame((_, delta) => {
-    if (ref.current) {
-      ref.current.rotation.x -= delta / 10;
-      ref.current.rotation.y -= delta / 15;
-    }
-  });
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta/10;
+    ref.current.rotation.y -= delta/15;
+  })
+
 
   return (
-    <group {...props} rotation={[0, 0, Math.PI / 4]}>
-      <Points
+    <group rotation={[0,0, Math.PI / 4]}>
+        <Points
         ref={ref}
         positions={sphere}
         stride={3}
         frustumCulled
-        // {...props}
-      >
-        <PointMaterial
-          transparent
-          color="#fff"
-          size={0.002}
-          sizeAttenuation={true}
-          depthWrite={false}
-        />
-      </Points>
+        {...props}
+        >
+            <PointMaterial
+                transparent
+                color="$fff"
+                size={0.002}
+                sizeAttenuation={true}
+                dethWrite={false}
+            />
+        </Points>
     </group>
-  );
+  )
 };
 
 const StarsCanvas = () => (
-  <div className="w-full h-auto fixed inset-0 z-[20]">
-    <Canvas camera={{ position: [0, 0, 1] }}>
-      <Suspense fallback={null}>
-        <StarBackground />
-      </Suspense>
-    </Canvas>
-  </div>
-);
+    <div className="w-full h-auto fixed inset-0 z-[20]">
+        <Canvas camera={{position: [0, 0, 1]}}>
+        <Suspense fallback={null}>
+            <StarBackground />
+        </Suspense>
+        </Canvas>
+    </div>
+)
 
 export default StarsCanvas;
