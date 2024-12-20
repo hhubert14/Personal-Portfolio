6bcf42ad9d2bda8dfd5fc9dd297edbd9c81838c2
hhubert14 <h.hubert@wustl.edu>
2024-12-19 19:59:59 -0600

    npm error with installing icons checkpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Hero from "../components/main/Hero";
+import Skills from "../components/main/Skills"
+import Encryption from "@/components/main/Encryption";
+import Projects from "@/components/main/Projects";
 
 export default function Home() {
     return (
       <main className="h-full w-full">
         <div className="flex flex-col gap-20">
           <Hero />
-          {/* <Skills />
+          <Skills />
           <Encryption />
-          <Projects /> */}
+          <Projects />
         </div>
       </main>
     );
